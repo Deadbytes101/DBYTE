226ff712f9d4d2ebe881621cb29a2268f5b8abd4
Deadbyte <deadbyte@dbyte.dev>
2026-05-11 10:32:01 +0700

    release: dbyte v1.8.0

--- crates/dbyte_cli/src/main.rs
+++ crates/dbyte_cli/src/main.rs
@@ -511,7 +511,7 @@ fn main() {
 
     match args[1].as_str() {
         "--version" => {
-            println!("DByte 1.7.1");
+            println!("DByte 1.8.0");
             process::exit(0);
         }
         "--help" | "-h" => {
